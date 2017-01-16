75b42f7ea0013ab24dab46fd16915076ebc27f40
Clive <clive21@outlook.com>
2017-01-16 07:50:08 +0530

    v4
    Added Behance portfolio

--- js/custom.js
+++ js/custom.js
@@ -47,21 +47,21 @@ jQuery(document).ready(function(){
 	/* ---------------------------------------------------------------------- */ 
 	
 	// Needed variables
-	var $container	 	= $('#portfolio-list');
-	var $filter 		= $('#portfolio-filter');
+	/*var $container	 	= $('#portfolio-list');
+	var $filter 		= $('#portfolio-filter');*/
 		
 	// Run Isotope  
-	$container.isotope({
+	/*$container.isotope({
 		filter				: '*',
 		layoutMode   		: 'masonry',
 		animationOptions	: {
 		duration			: 750,
 		easing				: 'linear'
 	   }
-	});	
+	});	*/
 	
 	// Isotope Filter 
-	$filter.find('a').click(function(){
+	/*$filter.find('a').click(function(){
 	  var selector = $(this).attr('data-filter');
 		$container.isotope({ 
 		filter				: selector,
@@ -72,34 +72,34 @@ jQuery(document).ready(function(){
 	   }
 	  });
 	  return false;
-	});	
+	});	*/
 	
 	// Portfolio image animation 
-	$container.find('img').adipoli({
+	/*$container.find('img').adipoli({
 		'startEffect' 	: 'normal',
 		'hoverEffect' 	: 'popout',
 		'imageOpacity' 	: 0.6,
 		'animSpeed' 	: 100,
-	});
+	});*/
 	
 	// Copy categories to item classes
-	$filter.find('a').click(function() {
+	/*$filter.find('a').click(function() {
 		var currentOption = $(this).attr('data-filter');
 		$filter.find('a').removeClass('current');
 		$(this).addClass('current');
-	});	
+	});	*/
 	
 	/* ---------------------------------------------------------------------- */
 	/*	Fancybox 
 	/* ---------------------------------------------------------------------- */
-	$container.find('.folio').fancybox({
+	/*$container.find('.folio').fancybox({
 		'transitionIn'		:	'elastic',
 		'transitionOut'		:	'elastic',
 		'speedIn'			:	200, 
 		'speedOut'			:	200, 
 		'overlayOpacity'	:   0.6
 	});
-	
+	*/
 	/* ---------------------------------------------------------------------- */
 	/*	Contact Form
 	/* ---------------------------------------------------------------------- */
@@ -176,3 +176,97 @@ jQuery(document).ready(function(){
 	
 
 });	
+
+
+
+
+
+
+$(function () {
+
+	var beUsername = 'clive21',
+		beApiKey = '2OP9FlG3eiW0dMEQbRQeTOwOOJeGeZ29',
+		bePerPage = 12,
+		beProjectAPI = 'http://www.behance.net/v2/users/' + beUsername + '/projects?callback=?&api_key=' + beApiKey + '&per_page=' + bePerPage,
+		beItemWidth = 360,
+		beItemHeight = 282,
+		beLazyLoad = true,
+		beLinkTarget = '_self';
+
+	/**
+	 * Get Data from Behance API
+	 */
+	if (sessionStorage.getItem('behanceProject')) {
+		setPortfolioTemplate();
+	} else {
+		// Load JSON-encoded data from the Behance API & Store it in sessionStorage
+		$.getJSON(beProjectAPI, function (project) {
+			sessionStorage.setItem('behanceProject', JSON.stringify(project));
+			setPortfolioTemplate();
+		});
+	}
+
+	/**
+	 * Populate Data
+	 */
+	function setPortfolioTemplate() {
+		var projects = JSON.parse(sessionStorage.getItem('behanceProject')).projects;
+		var portfolio = $('#portfolio-list').html('');
+		var items = '';
+		var image = '';
+
+		$.each(projects, function (i, val) {
+			// If Lazy load is enabled, edit the markup accordingly
+			beLazyLoad ? image = 'src="images/loading.png" data-lazy="' + val.covers.original + '"' : image = 'src="' + val.covers.original + '"';
+
+			// Create the items template
+			items += '<li class="be__item be__item__' + val.id + ' ">';
+			items += '<a href="' + val.url + '" title="' + val.name + '" target="' + beLinkTarget + '">';
+			items += '<img class="img-responsive" ' + image + ' width="' + beItemWidth + '" height="' + beItemHeight + '" alt="' + val.name + '">';
+			items += '</a>';
+			items += '</div>';
+
+			// How many items are shown
+			return i < bePerPage;
+		});
+
+		// Append items only once
+		portfolio.each(function (index, el) {
+			$(el).append(items);
+		});
+
+		// Create Lazy Load instance for Grid Layout
+		if (beLazyLoad) {
+			var layzr = new Layzr({
+				container: '.be__grid',
+				selector: '[data-lazy]',
+				attr: 'data-lazy'
+			});
+		}
+
+		
+	}
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
