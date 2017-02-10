961943663ea9d48b30046f3865a9d4476144bb6c
Clive <clive21@outlook.com>
2017-02-10 22:08:56 +0530

    v12
    Changed Profile pic and Style of Portfolio Display

--- js/b2w.js
+++ js/b2w.js
@@ -12,19 +12,19 @@
                         {query: 'screen and (min-width: 768px) and (max-width: 979px)', images: 12}, /* Change the number for total  thumbnails to display tablets in landscape  orientation */
                         {query: 'screen and (max-width: 767px)', images: 12} /* Change the number for total  thumbnails to display For lower resolution mobile devices */
                     ],
-                    onImagesLoaded: function(wrapper, data) {
+                  /*  onImagesLoaded: function(wrapper, data) {
                         $('li > a', wrapper).tooltip();
-                    },
+                    },*/
                     onDetailLoaded: function(wrapper, data) {
                         $('.prettyPhoto', wrapper).prettyPhoto({
                             social_tools: false
                         });
                     },
                     itemTemplate: '' +
-                            '<li class="mix {{#each fields}} {{replace this " " "-" }}{{/each}}" data-name="{{ name }}" data-publish="{{ published_on }}">' + '    <a id="behance-{{ id }}" href="{{ detailUrl }}" title="{{ name }}">' +
+                            '<li class="mix col-lg-3 col-md-4 col-sm-4 col-xs-12 {{#each fields}} {{replace this " " "-" }}{{/each}}" data-name="{{ name }}" data-publish="{{ published_on }}">' + '    <a id="behance-{{ id }}" href="{{ detailUrl }}" title="{{ name }}">' +
                             '        <img alt="{{ name }}" title="{{ name }}" src="{{ covers.[202] }}" class="cover-img">' +
                             '    </a>' +
-                            '    <span class="cover-tag">{{join fields ", " }}</span>' +
+                            '    <span class="project-title">{{ name }}</span>' +
                             '</li>',
                     detailTemplate: '' +
                             '<h1>{{ name }}</h1>' +
@@ -44,7 +44,7 @@
                             '        {{{ caption }}}' +
                             '        {{/if}}' +
                             '        ' +
-                            '            <img src="{{ src }}" width="{{ width }}" height="{{ height }}">' +
+                            '            <img src="{{ src }}" class="img-responsive">' +
                             '        ' +
                             '        {{else}}' +
                             '        {{{ text }}}' +
